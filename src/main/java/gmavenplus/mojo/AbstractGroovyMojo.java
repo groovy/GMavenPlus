ec6a10ac812b41d1249a8ca5af154895cf9a59c8
Keegan Witt <keeganwitt@gmail.com>
2011-09-17 10:41:14 -0400

    added check for jdk14 jars

--- src/main/java/gmavenplus/mojo/AbstractGroovyMojo.java
+++ src/main/java/gmavenplus/mojo/AbstractGroovyMojo.java
@@ -79,7 +79,9 @@ public abstract class AbstractGroovyMojo extends AbstractMojo {
         for (Object dependency : project.getCompileDependencies()) {
             Dependency dep = (Dependency) dependency;
             if ((dep.getGroupId().equals("org.codehaus.groovy") || dep.getGroupId().equals("groovy")) &&
-                    (dep.getArtifactId().equals("groovy-all") || dep.getArtifactId().equals("groovy-all-minimal") || dep.getArtifactId().equals("groovy")) &&
+                    (dep.getArtifactId().equals("groovy-all") || dep.getArtifactId().equals("groovy-all-minimal")
+                            || dep.getArtifactId().equals("groovy") || dep.getArtifactId().equals("groovy-all-jdk14")
+                            || dep.getArtifactId().equals("groovy-jdk14")) &&
                     dep.getType().equals("jar")) {
                 groovyVersion = dep.getVersion();
                 break;
