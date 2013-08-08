6d3704ea09bb88f3fdf86e6fe3cc5a8a3dfdc3fe
Keegan Witt <keeganwitt@gmail.com>
2013-08-08 18:59:20 -0400

    fix to allow access to Maven project object

--- src/main/java/org/codehaus/gmavenplus/mojo/ExecuteMojo.java
+++ src/main/java/org/codehaus/gmavenplus/mojo/ExecuteMojo.java
@@ -75,6 +75,7 @@ public class ExecuteMojo extends AbstractGroovyMojo {
 
             // create a GroovyShell to run scripts in
             Object shell = ReflectionUtils.invokeConstructor(ReflectionUtils.findConstructor(groovyShellClass));
+            ReflectionUtils.invokeMethod(ReflectionUtils.findMethod(groovyShellClass, "setProperty", String.class, Object.class), shell, "project", project);
 
             // TODO: load runtime project dependencies onto classpath before executing so they can be used in scripts?
 
