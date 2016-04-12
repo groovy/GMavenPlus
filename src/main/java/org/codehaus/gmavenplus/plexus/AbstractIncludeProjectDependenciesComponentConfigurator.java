cb1fa4f905ca83f9933adc8bdbea3bed54925a53
Keegan Witt <keeganwitt@gmail.com>
2016-04-11 23:10:56 -0400

    clarified AbstractIncludeProjectDependenciesComponentConfigurator license

--- src/main/java/org/codehaus/gmavenplus/plexus/AbstractIncludeProjectDependenciesComponentConfigurator.java
+++ src/main/java/org/codehaus/gmavenplus/plexus/AbstractIncludeProjectDependenciesComponentConfigurator.java
@@ -1,5 +1,17 @@
 /*
- * Originally from http://stackoverflow.com/questions/2659048/add-maven-build-classpath-to-plugin-execution-classpath
+ * Copyright (C) 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.codehaus.gmavenplus.plexus;
@@ -25,7 +37,7 @@ import java.util.List;
  * Note that there are two method signatures for addDependenciesToClassRealm,
  * since different versions of Maven have different expectations.
  *
- * @author Brian Jackson
+ * @author <a href="http://stackoverflow.com/a/2659324/160256">Brian Jackson</a>
  * @author Keegan Witt
  * @since 1.0-beta-3
  */
