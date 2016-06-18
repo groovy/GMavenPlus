51fd14c1a1e19156fe143762fa7e3396fa60a9be
Keegan Witt <keeganwitt@gmail.com>
2016-06-17 22:07:19 -0400

    improved groovydoc integration tests

--- src/it/advancedGroovydoc/src/test/java/org/codehaus/gmavenplus/TheTest.java
+++ src/it/advancedGroovydoc/src/test/java/org/codehaus/gmavenplus/TheTest.java
@@ -20,29 +20,48 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 
 public class TheTest {
 
     @Test
-    public void testSomeClassExists() {
+    public void testSomeClassExistsAndContainsJavaLinks() throws Exception {
         File generatedGroovyDoc = new File("target/gapidocs/org/codehaus/gmavenplus/SomeClass.html");
         Assert.assertTrue(generatedGroovyDoc + " does not exist.", generatedGroovyDoc.exists());
         Assert.assertTrue(generatedGroovyDoc + " is empty.", generatedGroovyDoc.length() > 0);
+        Assert.assertTrue(generatedGroovyDoc + " does not contain Java 5 links.", readFileToString(generatedGroovyDoc)
+                .contains("http://docs.oracle.com/javase/5/docs/api/"));
     }
 
     @Test
-    public void testSomeOtherClassExists() {
+    public void testSomeOtherClassExistsAndContainsJavaLinks() throws Exception {
         File generatedGroovyDoc = new File("target/gapidocs/org/codehaus/gmavenplus/SomeOtherClass.html");
         Assert.assertTrue(generatedGroovyDoc + " does not exist.", generatedGroovyDoc.exists());
         Assert.assertTrue(generatedGroovyDoc + " is empty.", generatedGroovyDoc.length() > 0);
+        Assert.assertTrue(generatedGroovyDoc + " does not contain Java 5 links.", readFileToString(generatedGroovyDoc)
+                .contains("http://docs.oracle.com/javase/5/docs/api/"));
     }
 
     @Test
-    public void testOverviewSummaryExists() {
+    public void testOverviewSummaryExistsAndContainsStylesheetLink() throws Exception {
         File generatedGroovyDoc = new File("target/gapidocs/overview-summary.html");
         Assert.assertTrue(generatedGroovyDoc + " does not exist.", generatedGroovyDoc.exists());
         Assert.assertTrue(generatedGroovyDoc + " is empty.", generatedGroovyDoc.length() > 0);
+        Assert.assertTrue(generatedGroovyDoc + " does not contain stylesheet link.", readFileToString(generatedGroovyDoc)
+                .contains("<link rel =\"stylesheet\" type=\"text/css\" href=\"stylesheet.css\" title=\"Style\">"));
     }
 
+    private String readFileToString(File file) throws IOException {
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+            return scanner.useDelimiter("\\A").next();
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+    }
 }
