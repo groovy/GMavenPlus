b454fe000084d7c14d919ca6e9a673e8b634806c
Keegan Witt <keeganwitt@gmail.com>
2022-06-13 12:44:00 -0400

    Invert version check in test to make maintenance easier

--- src/it/parametersCompile/src/test/java/org/codehaus/gmavenplus/SomeClassTest.java
+++ src/it/parametersCompile/src/test/java/org/codehaus/gmavenplus/SomeClassTest.java
@@ -34,14 +34,22 @@ public class SomeClassTest {
         List<Parameter> parameterNames = Arrays.asList(method.getParameters());
 
         Assert.assertEquals(2, parameterNames.size());
-        if (GroovySystem.getVersion().startsWith("2.5")
-                || GroovySystem.getVersion().startsWith("3.0")
-                || GroovySystem.getVersion().startsWith("4.0")) {
-            Assert.assertEquals("param1", parameterNames.get(0).getName());
-            Assert.assertEquals("param2", parameterNames.get(1).getName());
-        } else {
+
+        if (GroovySystem.getVersion().startsWith("1.5")
+                || GroovySystem.getVersion().startsWith("1.6")
+                || GroovySystem.getVersion().startsWith("1.7")
+                || GroovySystem.getVersion().startsWith("1.8")
+                || GroovySystem.getVersion().startsWith("1.9")
+                || GroovySystem.getVersion().startsWith("2.0")
+                || GroovySystem.getVersion().startsWith("2.1")
+                || GroovySystem.getVersion().startsWith("2.2")
+                || GroovySystem.getVersion().startsWith("2.3")
+                || GroovySystem.getVersion().startsWith("2.4")) {
             Assert.assertEquals("arg0", parameterNames.get(0).getName());
             Assert.assertEquals("arg1", parameterNames.get(1).getName());
+        } else {
+            Assert.assertEquals("param1", parameterNames.get(0).getName());
+            Assert.assertEquals("param2", parameterNames.get(1).getName());
         }
     }
 
