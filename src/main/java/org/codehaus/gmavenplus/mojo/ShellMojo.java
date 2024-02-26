bebc06d052f86838a04ca3d161882c4a437d2a61
Keegan Witt <keeganwitt@gmail.com>
2024-02-26 15:23:45 -0500

    Added a warning about Security Manager deprecation and handle exception in Java 17+ (closes #286)

--- src/main/java/org/codehaus/gmavenplus/mojo/ShellMojo.java
+++ src/main/java/org/codehaus/gmavenplus/mojo/ShellMojo.java
@@ -91,10 +91,15 @@ public class ShellMojo extends AbstractToolsMojo {
         }
 
         if (groovyVersionSupportsAction()) {
-            final SecurityManager sm = System.getSecurityManager();
+            final SecurityManager defaultSecurityManager = System.getSecurityManager();
             try {
                 if (!allowSystemExits) {
-                    System.setSecurityManager(new NoExitSecurityManager());
+                    getLog().warn("JEP 411 deprecated Security Manager in Java 17 for removal. Therefore `allowSystemExits` is also deprecated for removal.");
+                    try {
+                        System.setSecurityManager(new NoExitSecurityManager());
+                    } catch (UnsupportedOperationException e) {
+                        getLog().warn("Attempted to use Security Manager in a JVM where it's disabled by default. You might try `-Djava.security.manager=allow` to override this.");
+                    }
                 }
 
                 // get classes we need with reflection
@@ -123,7 +128,11 @@ public class ShellMojo extends AbstractToolsMojo {
                 throw new MojoExecutionException("Error occurred while instantiating a Groovy class from classpath.", e);
             } finally {
                 if (!allowSystemExits) {
-                    System.setSecurityManager(sm);
+                    try {
+                        System.setSecurityManager(defaultSecurityManager);
+                    } catch (UnsupportedOperationException e) {
+                        getLog().warn("Attempted to use Security Manager in a JVM where it's disabled by default. You might try `-Djava.security.manager=allow` to override this.");
+                    }
                 }
             }
         } else {
